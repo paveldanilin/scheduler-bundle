821f04d62e6539b2ab9561fc817a46945165b05f
Pavel <pavel.k.danilin@gmail.com>
2022-03-06 16:46:19 +1000

    Add PsrLogMessageProcessor

--- Logger/LoggerFactory.php
+++ Logger/LoggerFactory.php
@@ -4,6 +4,7 @@ namespace Pada\SchedulerBundle\Logger;
 
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Logger;
+use Monolog\Processor\PsrLogMessageProcessor;
 use Psr\Log\LoggerInterface;
 
 abstract class LoggerFactory
@@ -11,8 +12,11 @@ abstract class LoggerFactory
     public static function createLogger(string $logDir): LoggerInterface
     {
         $logger = new Logger('scheduler');
-        $rotatingHandler = new RotatingFileHandler($logDir . DIRECTORY_SEPARATOR . 'scheduler.log', 30, Logger::DEBUG);
+        $rotatingHandler = new RotatingFileHandler($logDir . DIRECTORY_SEPARATOR . 'scheduler.log',
+            30,
+            Logger::DEBUG);
         $logger->pushHandler($rotatingHandler);
+        $logger->pushProcessor(new PsrLogMessageProcessor());
         return $logger;
     }
 }
